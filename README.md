84bf7d4bfce832013ad9bcaa0285c3db2b37794b
Alex Chan <alex@alexwlchan.net>
2024-08-19 13:20:44 +0100

    Start building the basic set of command-line flags

---
+++ README.md
@@ -0,0 +1,1 @@
+create-thumbnail PATH [--width=WIDTH | --height=HEIGHT] --out-dir=OUT_DIR
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::path::PathBuf;
+
+use clap::{ArgGroup, Parser};
+
+#[derive(Debug, Parser)]
+#[clap(version, about)]
+#[clap(group(
+  ArgGroup::new("dimensions")
+      .required(true)
+      .args(&["height", "width"]),
+))]
+struct Cli {
+    /// Path to the image to be thumbnailed
+    path: PathBuf,
+
+    /// Path to the directory to save the thumbnail in
+    #[arg(long)]
+    out_dir: PathBuf,
+
+    /// Height of the thumbnail to create
+    #[arg(long)]
+    height: Option<usize>,
+
+    /// Width of the thumbnail to create
+    #[arg(long)]
+    width: Option<usize>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("args = {:?}", cli);
 }
